86c72638a982178da6bc94a3ce197897f7bee46e
Hyeondoonge <migu554@naver.com>
2022-02-18 16:40:38 +0900

    feat: 검색 창 스타일링, debounce 적용

--- src/components/SearchArea.tsx
+++ src/components/SearchArea.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { RiSearchLine } from 'react-icons/ri';
 
 interface SearchAreaProps {
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
@@ -7,20 +8,32 @@ interface SearchAreaProps {
 
 const StyledSearchArea = styled.div`
   width: 100%;
+  border: 1px solid rgb(201, 202, 204);
+  border-radius: 4px;
+  display: flex;
+  align-items: center;
+  background-color: white;
 `;
 
 const StyledInput = styled.input`
   width: 100%;
+  margin: 12px 0px;
+  border: none;
+
+  &:focus {
+    outline: none;
+  }
+
+  &::placeholer {
+    color: grey;
+  }
 `;
 
 export default function SearchArea({ onChange }: SearchAreaProps) {
   return (
-    <div>
-      <StyledInput
-        type="text"
-        placeholder="배우고 싶은 언어, 기술을 선택하세요"
-        onChange={onChange}
-      />
-    </div>
+    <StyledSearchArea>
+      <RiSearchLine style={{ margin: '0px 16px' }} />
+      <StyledInput type="text" placeholder="배우고 싶은 언어, 기술을 선택하세요" onChange={onChange} />
+    </StyledSearchArea>
   );
 }
