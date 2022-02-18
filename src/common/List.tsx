f24130ed416487125b5903e6be00febff4428af8
Hyeondoonge <migu554@naver.com>
2022-02-18 14:02:22 +0900

    design: 공통 컴포넌트 List 생성

---
+++ src/common/List.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+interface ListProps {
+  children: React.ReactNode;
+}
+const StyledList = styled.div`
+  flex-wrap: wrap;
+  display: flex;
+  position: relative;
+`;
+
+export default function List({ children }: ListProps) {
+  return <StyledList>{children}</StyledList>;
+}
