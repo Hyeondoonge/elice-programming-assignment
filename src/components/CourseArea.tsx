6af7c83f3ec043fd125ffb95e6d4d82288abb6a4
Hyeondoonge <migu554@naver.com>
2022-02-18 14:10:03 +0900

    design: 강좌(카드) 컴포넌트 생성

--- src/components/CourseArea.tsx
+++ src/components/CourseArea.tsx
@@ -1,10 +1,50 @@
 import React from 'react';
 import styled from 'styled-components';
+import List from '../common/List';
 
 const StyledCourseArea = styled.div`
   width: 100%;
 `;
 
+interface CardProps {
+  data: {
+    title: string;
+    description: string;
+    logo: string;
+    label: string;
+  };
+}
+
+const StyledCard = styled.div``;
+
+const Card = ({ data }: CardProps) => {
+  const { title, description, logo, label } = data;
+
+  return (
+    <StyledCard>
+      <div>{title}</div>
+      <div>{description}</div>
+      <div>{logo}</div>
+      <div>{label}</div>
+    </StyledCard>
+  );
+};
+
 export default function CourseArea() {
-  return <StyledCourseArea>강좌목록영역</StyledCourseArea>;
+  const data = {
+    title: 'docker',
+    description: '쉬워요',
+    logo: '??',
+    label: '무료'
+  };
+
+  return (
+    <StyledCourseArea>
+      강좌목록영역
+      <List>
+        <Card data={data} />
+        <Card data={data} />
+      </List>
+    </StyledCourseArea>
+  );
 }
