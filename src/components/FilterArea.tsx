2112e0e2d1eb2d88a61aa40c89c3adba2da979f5
Hyeondoonge <migu554@naver.com>
2022-02-18 21:24:45 +0900

    feat: 필터 클릭 이벤트 핸들
    - 선택된 필터 상태 업데이트
    - 새로운 필터에 따른 새로운 강좌 상태 업데이트

--- src/components/FilterArea.tsx
+++ src/components/FilterArea.tsx
@@ -1,38 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import Chip from '../common/Chip';
 import List from '../common/List';
 
 interface FilterAreaProps {
-  onClick: (e: React.MouseEvent<HTMLInputElement>) => void;
+  onClickHandler: (filters: Array<string>) => void;
 }
-
-interface ChipProps {
-  label: string;
-  // onClick: (e: React.MouseEvent<HTMLInputElement>) => void;
-}
-
-const StyledChip = styled.div`
-  cursor: pointer;
-`;
-
-const Chip = ({ label }: ChipProps) => {
-  return <StyledChip>{label}</StyledChip>;
-};
 
 const StyledFilterArea = styled.div`
   width: 100%;
 `;
 
-export default function FilterArea({ onClick }: FilterAreaProps) {
+const typeList = ['가격'];
+
+const StyledRow = styled.div`
+  display: flex;
+  align-items: center;
+  border: 1px solid #d8d8d8;
+`;
+
+const StyledType = styled.div`
+  background-color: #e8e8e8;
+  border: 1px solid #d8d8d8;
+  padding: 10px;
+  width: 10%;
+`;
+
+interface FilterProps {
+  name: string;
+  selected: boolean;
+}
+
+type Filter = '가격';
+
+export default function FilterArea({ onClickHandler }: FilterAreaProps) {
+  const [filterList, setFilterList] = useState({
+    가격: [
+      { name: '무료', selected: false },
+      { name: '유료', selected: false }
+    ]
+  });
+
   return (
     <StyledFilterArea>
-      필터영역
-      <div>
-        <List>
-          <Chip label={'필터1'} />
-          <Chip label={'필터2'} />
-        </List>
-      </div>
+      {typeList.map((type: Filter, index) => (
+        <StyledRow key={index}>
+          <StyledType>{type}</StyledType>
+          <div
+            style={{
+              width: '100%'
+            }}
+          >
+            <List>
+              {filterList[type].map(({ name, selected }: FilterProps, index: number) => (
+                <div key={index} style={{ margin: '5px' }}>
+                  <Chip
+                    label={name}
+                    selected={selected}
+                    onClick={() => {
+                      const newFilterList = { ...filterList };
+                      newFilterList[type][index].selected = !newFilterList[type][index].selected;
+                      // update 아이템 클릭 상태
+                      // i, j를 전달해서 상태값 업데이트 진행 및 fetch 수행
+                      setFilterList(newFilterList);
+                      onClickHandler(newFilterList[type].filter(({ selected }) => selected).map(({ name }) => name));
+                    }}
+                  />
+                </div>
+              ))}
+            </List>
+          </div>
+        </StyledRow>
+      ))}
     </StyledFilterArea>
   );
 }
