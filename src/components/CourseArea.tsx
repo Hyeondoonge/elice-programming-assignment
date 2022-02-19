d6ddd8570465eb56be88318f6e451a84cd875e0a
Hyeondoonge <migu554@naver.com>
2022-02-19 12:38:09 +0900

    design: 멀티라인 생략 기호 추가, 아이콘 다양하게 추가

--- src/components/CourseArea.tsx
+++ src/components/CourseArea.tsx
@@ -1,20 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
-import List from '../common/List';
-import { RiBarChartLine } from 'react-icons/ri';
+import { RiBarChartLine, RiDashboardFill, RiFeedbackFill } from 'react-icons/ri';
 import { CourseProps, CoursesProps } from '../components/type';
 
-const StyledCard = styled.div`
-  width: 260px;
-  height: 300px;
-  border-radius: 8px;
-  padding: 28px 24px;
-  background-color: white;
-  position: relative;
-  display: grid;
-  grid-template-rows: 10% 20% 20%;
+const EllipsisWrapper = styled.div`
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 2;
+  line-height: 1.6;
 `;
-
 interface IconTextProps {
   Icon: any; // IconType
   text: string;
@@ -26,6 +22,18 @@ const IconText = ({ Icon, text }: IconTextProps) => (
     <span style={{ fontSize: 12, color: '#7d7e80' }}>{text}</span>
   </div>
 );
+
+const StyledCard = styled.div`
+  width: 260px;
+  height: 300px;
+  border-radius: 8px;
+  padding: 28px 24px;
+  background-color: white;
+  position: relative;
+  display: grid;
+  grid-template-rows: 5% 15% 15%;
+  row-gap: 30px;
+`;
 
 const Card = (course: CourseProps) => {
   const { title, short_description, logo_file_url, enroll_type, is_free } = course;
@@ -41,23 +49,17 @@ const Card = (course: CourseProps) => {
   return (
     <StyledCard>
       <div style={{ display: 'flex', alignItems: 'center' }}>{label}</div>
-      <div style={{ fontWeight: 600 }}>{title}</div>
-      <div
-        style={{
-          color: '#5e5f61',
-          lineHeight: 1.6,
-          textOverflow: 'ellipsis',
-          overflow: 'hidden',
-          whiteSpace: 'nowrap'
-        }}
-      >
-        <span style={{ fontSize: 14 }}>{short_description}</span>
-      </div>
+      <EllipsisWrapper>
+        <span style={{ fontWeight: 600, color: '#222', fontSize: '16px' }}>{title}</span>
+      </EllipsisWrapper>
+      <EllipsisWrapper>
+        <span style={{ color: '#5e5f61', fontSize: '14px' }}>{short_description}</span>
+      </EllipsisWrapper>
       <div style={{ display: 'flex', justifyContent: 'space-between' }}>
         <div style={{ display: 'flex', flexDirection: 'column', gap: 5 }}>
-          <IconText Icon={() => <RiBarChartLine />} text={'난이도: 미설정'} />
-          <IconText Icon={() => <RiBarChartLine />} text={'수업 : 온라인'} />
-          <IconText Icon={() => <RiBarChartLine />} text={'기간 : 무제한'} />
+          <IconText Icon={RiBarChartLine} text={'난이도: 미설정'} />
+          <IconText Icon={RiDashboardFill} text={'수업 : 온라인'} />
+          <IconText Icon={RiFeedbackFill} text={'기간 : 무제한'} />
         </div>
         <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
           <img style={{ objectFit: 'cover' }} width={52} height={52} src={logo_file_url} />
