118a4f0b151520898caf3653de2c3012d060dc58
Hyeondoonge <migu554@naver.com>
2022-04-06 17:11:04 +0900

    update: 로딩 상태 추가, course-totalCount 상태 한 번에 관리

--- src/hooks/useGetCourse.ts
+++ src/hooks/useGetCourse.ts
@@ -2,39 +2,51 @@ import { useEffect, useState } from 'react';
 import { fetchCourse } from '../api/courseAPI';
 import { CourseProps, OptionProps } from '../type';
 
-type ReturnTypes = [CourseProps[], number, Function];
+interface DataProps {
+  courses: CourseProps[];
+  totalCount: number;
+}
 
-export default function useGetCourse(): ReturnTypes {
-  const [courses, setCourses] = useState<CourseProps[]>([]);
-  const [totalCount, setTotalCount] = useState(0);
+interface hookProps {
+  (): [DataProps, (options: OptionProps) => void, boolean];
+}
+
+export const useGetCourse: hookProps = function (): [
+  DataProps,
+  (options: OptionProps) => void,
+  boolean
+] {
+  const [data, setData] = useState<DataProps>({ totalCount: 0, courses: [] });
+  const [loading, setLoading] = useState(false);
   const count = 20;
 
-  const updateCourses = (options: OptionProps) => {
+  const updateCourses = async (options: OptionProps) => {
     try {
-      (async () => {
-        const { title, price, offset } = options;
+      setLoading(true);
+      const { title, price, offset } = options;
 
-        if (!title) {
-          setTotalCount(0);
-          setCourses([]);
-          return;
-        }
+      const param = {
+        filter_conditions: {
+          $and: [
+            { title: `%${title}%` },
+            {
+              $or: price?.map(name =>
+                name === '무료'
+                  ? { enroll_type: 0, is_free: true }
+                  : { enroll_type: 0, is_free: false }
+              )
+            }
+          ]
+        },
+        offset,
+        count
+      };
 
-        const param = {
-          filter_conditions: {
-            $and: [
-              { title: `%${title}%` },
-              { $or: price?.map(name => (name === '무료' ? { enroll_type: 0, is_free: true } : { enroll_type: 0, is_free: false })) }
-            ]
-          },
-          offset,
-          count
-        };
-
-        const res = await fetchCourse(param);
-        setTotalCount(res.course_count);
-        setCourses(
-          res.courses.map(({ id, title, short_description, logo_file_url, label, enroll_type, is_free }: CourseProps) => ({
+      const res = await fetchCourse(param);
+      setData({
+        totalCount: res.course_count,
+        courses: res.courses.map(
+          ({
             id,
             title,
             short_description,
@@ -42,13 +54,22 @@ export default function useGetCourse(): ReturnTypes {
             label,
             enroll_type,
             is_free
-          }))
-        );
-      })();
+          }: CourseProps) => ({
+            id,
+            title,
+            short_description,
+            logo_file_url,
+            label,
+            enroll_type,
+            is_free
+          })
+        )
+      });
+      setLoading(false);
     } catch (error) {
       throw new Error(error);
     }
   };
 
-  return [courses, totalCount, updateCourses];
-}
+  return [data, updateCourses, loading];
+};
